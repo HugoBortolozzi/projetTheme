6d8213894451356c05449d93a0d47bd155dd8e1b
Hugo Bortolozzi <hugo_bortolozzi@yahoo.fr>
2019-08-14 14:50:10 +0200

    ajout partial statistics

--- index.php
+++ index.php
@@ -24,6 +24,9 @@ get_template_part('templates/screenshots');
 
 //Ajout de la section download
 get_template_part('templates/download');
+
+//Ajout de la section statistics
+get_template_part('templates/statistics');
 ?>
 
     
@@ -37,38 +40,7 @@ get_template_part('templates/download');
     
 
 
-    <!-- Statistics -->
-    <div class="counter">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    
-                    <!-- Counter -->
-                    <div id="counter">
-                        <div class="cell">
-                            <div class="counter-value number-count" data-count="231">1</div>
-                            <p class="counter-info">Happy Users</p>
-                        </div>
-                        <div class="cell">
-                            <div class="counter-value number-count" data-count="85">1</div>
-                            <p class="counter-info">Issues Solved</p>
-                        </div>
-                        <div class="cell">
-                            <div class="counter-value number-count" data-count="59">1</div>
-                            <p class="counter-info">Good Reviews</p>
-                        </div>
-                        <div class="cell">
-                            <div class="counter-value number-count" data-count="127">1</div>
-                            <p class="counter-info">Case Studies</p>
-                        </div>
-                    </div>
-                    <!-- end of counter -->
-                    
-                </div> <!-- end of col -->
-            </div> <!-- end of row -->
-        </div> <!-- end of container -->
-    </div> <!-- end of counter -->
-    <!-- end of statistics -->
+    
 
 
     <!-- Contact -->
