11a99f59525777096d4e4eba8b843e05376cdfaf
Hugo Bortolozzi <hugo_bortolozzi@yahoo.fr>
2019-08-14 16:06:03 +0200

    Ajout fonctions wp_enqueue_script(); ( ici ça merde hard)

--- functions.php
+++ functions.php
@@ -15,18 +15,21 @@ function ajout_css_js()
   wp_enqueue_style('magnific-popup', get_template_directory_uri() . '/css/magnific-popup.css');
   wp_enqueue_style('swiper', get_template_directory_uri() . '/css/swiper.css');
   wp_enqueue_style('style', get_template_directory_uri() . '/css/style.css');
+
+  // Ajout des scripts js
+  // https://developer.wordpress.org/reference/functions/wp_enqueue_script/
+  wp_enqueue_script('jquery-perso', get_template_directory_uri() . '/js/jquery.min.js', null, true);
+  wp_enqueue_script('popper', get_template_directory_uri() . '/js/popper.min.js', ['jquery-perso'], null, true);
+  wp_enqueue_script('bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', ['jquery-perso'], null, true);
+  wp_enqueue_script('jquery-easing', get_template_directory_uri() . '/js/jquery.easing.min.js', ['jquery-perso'], null, true);
+
+  wp_enqueue_script('swiper', get_template_directory_uri() . '/js/swiper.min.js', ['jquery-perso'], null, true);
+  wp_enqueue_script('magnific-popup', get_template_directory_uri() . '/js/jquery.magnific-popup.js', ['jquery-perso'], null, true);
+  wp_enqueue_script('morphext', get_template_directory_uri() . '/js/morphext.min.js', ['jquery-perso'], null, true);
+  wp_enqueue_script('validator', get_template_directory_uri() . '/js/validator.min.js', ['jquery-perso'], null, true);
+  wp_enqueue_script('script', get_template_directory_uri() . '/js/scripts.js', ['jquery-perso'], null, true);
 }
 // Nous ajoutons un écouteur d'événements pour nous prévenir lorsque l'on peut ajouter des css et scripts.
 // Cette écouteur va déclancher la fonction ajout_css_js()
 // https://developer.wordpress.org/reference/hooks/wp_enqueue_scripts/
 add_action('wp_enqueue_scripts', 'ajout_css_js');
-?>
-
-
-<link href="https://fonts.googleapis.com/css?family=Montserrat:400,600,700" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i" rel="stylesheet">
-    <link href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.css" rel="stylesheet">
-    <link href="<?php echo get_template_directory_uri(); ?>/css/fontawesome-all.css" rel="stylesheet">
-    <link href="<?php echo get_template_directory_uri(); ?>/css/swiper.css" rel="stylesheet">
-	<link href="<?php echo get_template_directory_uri(); ?>/css/magnific-popup.css" rel="stylesheet">
-	<link href="<?php echo get_template_directory_uri(); ?>/css/styles.css" rel="stylesheet">
