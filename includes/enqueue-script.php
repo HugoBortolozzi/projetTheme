9210fc2440fe3a9ec9d6810b5e4fec96790cc12f
Hugo Bortolozzi <hugo_bortolozzi@yahoo.fr>
2019-08-19 14:20:37 +0200

    introduction de la notion orienté objet

--- includes/enqueue-script.php
+++ includes/enqueue-script.php
@@ -1,10 +1,13 @@
 <?php
-/**
- * Fonction qui va ajouter des scripts dynamiquement afin que l'on puisse les inclures dans le thème avec wp_head() et wp_footer()
- *
- * @return void
- */
-function ajout_css_js()
+class EnqueueScript
+{
+  /**
+   * Fonction qui va ajouter des scripts dynamiquement afin que l'on puisse les inclures dans le thème avec wp_head() et wp_footer()
+   * Nous avons ajouter le mot public afin que cette méthode puisse être utiliser depuis l'exterieur. Cela veut dire que l'on peut créer une instance de cette class et puis faire appel à la méthode ( ex: $instance->methode() )
+   *
+   * @return void
+   */
+  public function ajout_css_js()
 {
   // Ajout des scripts css
   // https://developer.wordpress.org/reference/functions/wp_enqueue_style/
@@ -28,8 +31,13 @@ function ajout_css_js()
   wp_enqueue_script('morphext', get_template_directory_uri() . '/js/morphext.min.js', ['jquery-perso'], null, true);
   wp_enqueue_script('validator', get_template_directory_uri() . '/js/validator.min.js', ['jquery-perso'], null, true);
   wp_enqueue_script('script', get_template_directory_uri() . '/js/scripts.js', ['jquery-perso'], null, true);
+  }
 }
 // Nous ajoutons un écouteur d'événements pour nous prévenir lorsque l'on peut ajouter des css et scripts.
 // Cette écouteur va déclancher la fonction ajout_css_js()
 // https://developer.wordpress.org/reference/hooks/wp_enqueue_scripts/
-add_action('wp_enqueue_scripts', 'ajout_css_js');
+
+// Nous créeons une instance de la class EnqueueScript afin de la passer en paramètre dans notre add_action
+$enqueue_script = new EnqueueScript();
+// la function add_action prend en deuxième paramêtre soit un string (qui correspond au nom d'une fonction), soit un tableau. Dans le tableau on passe en premier paramêtre l'un objet instance d'une class et en deuxième paramêtre un string qui correspond au nom de la méthode de l'objet passé en premier paramêtre.
+add_action('wp_enqueue_scripts', [$enqueue_script, 'ajout_css_js']);
